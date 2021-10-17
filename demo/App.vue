766c224d30fc67039640226b73041263f121b04e
zlcatwu <zlcatwu@163.com>
2021-10-17 22:50:10 +0800

    chore: initial commit

---
+++ demo/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <TestTable :test="test">
+      <span>xxxxxx</span>
+    </TestTable>
+  </div>
+</template>
+
+<script lang="ts">
+import { TestTable } from '../src/table'
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    TestTable,
+  },
+  setup() {
+    const test = ref(true)
+
+    return { test }
+  },
+})
+</script>
---
+++ src/table/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by uedc on 2021/10/11.
+ */
+
+import TestTable from './Table'
+
+export { TestTable }
